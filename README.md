4a39cca7f572923fb60be546658206934f3b8fcc
Lars Bolvig <larsbolvig@gmail.com>
2020-04-22 11:22:11 +0200

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# larsbolvig.github.io
---
+++ precache-manifest.bc46ee142b6ff3537255f6cfa1d5cf42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f80900bfa354bd714c14cecf0e7ebb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84f0c0e3c03fe97aa576",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "00e9a092c8fa53920df3",
+    "url": "/static/js/2.7c0c6af9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c0c6af9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f0c0e3c03fe97aa576",
+    "url": "/static/js/main.e0f71c5d.chunk.js"
+  },
+  {
+    "revision": "86bd4491d36b91b4e8bb",
+    "url": "/static/js/runtime-main.1c6f25a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
